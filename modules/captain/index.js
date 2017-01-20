89513b94b1476643a714b6a60dd41c294af37f61
Kevin Balicot <kevin.balicot@port-djw6fp4j.abc-objectif.local>
2017-01-20 11:24:33 +0100

    Add some stuff for captain, he says curses and songs

--- modules/captain/index.js
+++ modules/captain/index.js
@@ -1,6 +1,9 @@
 const utils = require('./../../services/utils');
+const songs = require('./songs.json');
+const curses = require('./curses.json');
 
 const words = ['Aaar!', 'Oy oy', 'Yaarr!', 'Yo ho ho', 'Ahoy!', 'Yaarrahah!'];
+const beers = ['🍺', '🍻', 'beer', 'bière', 'biere', 'apéro', 'apero'];
 
 module.exports = client => {
     client.on('message', message => {
@@ -9,8 +12,30 @@ module.exports = client => {
             return;
         }
 
-        const hasBotMention = message.mentions.users.array().filter(user => user.id == client.user.id);
-        if (hasBotMention.length > 0) {
+        const hasBotMention = (message.mentions.users.array().filter(user => user.id == client.user.id)).length > 0;
+
+        if (utils.matchOneOf(beers, message.content)) {
+            message.reply(utils.getRandomlyOneOf(words) + ' 🍺');
+        }
+
+        if (hasBotMention && utils.matchOneOf(['chanson', 'chante'], message.content)) {
+            const song = utils.getRandomlyOneOf(songs);
+            song.forEach(sentence => message.channel.sendMessage(sentence));
+
+            return;
+        }
+
+        if (hasBotMention && utils.matchOneOf(['corrige', 'insulte', 'puni', 'vilipende'], message.content)) {
+            const users = message.mentions.users.array().filter(user => user.id != client.user.id);
+            users.forEach(user => {
+                const curse = utils.getRandomlyOneOf(curses);
+                message.channel.sendMessage(`<@${user.id}> ${curse} !`);
+            });
+
+            return;
+        }
+
+        if (hasBotMention) {
             message.reply(utils.getRandomlyOneOf(words));
         }
     });
