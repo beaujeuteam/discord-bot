ab5d8c1d9a879f22372d0552cc41866b70f38685
Kevin Balicot <kevinbalicot@gmail.com>
2018-04-16 12:35:06 +0200

    fix(fun): Fix kaamelott command, add character option

--- modules/kaamelott/index.js
+++ modules/kaamelott/index.js
@@ -2,9 +2,10 @@ const utils = require('./../../services/utils');
 const { Command } = require('./../../services/commands');
 const request = require('request');
 
-// @see http://kaamelott.underfloor.io/doc
-const apiUrl = 'http://kaamelott.underfloor.io/quote/rand';
+// @see https://github.com/sin0light/api-kaamelott/
+const apiUrl = 'https://kaamelott.chaudie.re/api/random';
 const kaamelottCmd = new Command('kaamelott', 'Display random kaamelott quote.', 'fun');
+const kaamelottPersoCmd = new Command('kaamelott [character]', 'Display random kaamelott quote from personnage.', 'fun');
 
 module.exports = client => {
     client.on('message', message => {
@@ -13,12 +14,25 @@ module.exports = client => {
             return;
         }
 
+        kaamelottPersoCmd.match(message.content, ({ character }) => {
+            const url = `${apiUrl}/personnage/${character}`;
+            request(url, (err, res, body) => {
+                client.logger.request(url, res);
+
+                if (!err && res.statusCode == 200) {
+                    const { infos, citation } = JSON.parse(body).citation;
+                    message.channel.send(`**${infos.personnage}** _(${infos.saison} ${infos.episode})_\n${citation.trim()}`);
+                }
+            });
+        });
+
         kaamelottCmd.match(message.content, () => {
             request(apiUrl, (err, res, body) => {
                 client.logger.request(apiUrl, res);
 
                 if (!err && res.statusCode == 200) {
-                    message.channel.send(JSON.parse(body).quote || 'C\'est pas faux.');
+                    const { infos, citation } = JSON.parse(body).citation;
+                    message.channel.send(`**${infos.personnage}** _(${infos.saison} ${infos.episode})_\n${citation.trim()}`);
                 }
             });
         });
